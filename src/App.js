0096f1862bd19b7152da6f67c915e9222e41f212
icallas <iyad.callas@epitech.eu>
2021-07-01 11:28:03 +0200

    front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { Form } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="centered">
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicDestination">
+          <Form.Label>Destination</Form.Label>
+          <Form.Control size="lg" type="text" placeholder="Enter Destination" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicDate">
+          <Form.Label>Date</Form.Label>
+          <Form.Control size="lg" type="date" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicActivities">
+          <Form.Label>Activities</Form.Label>
+          <Form.Control size="lg" type="text" placeholder="Select Activities" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicActivities">
+          <Form.Label>Number of People</Form.Label>
+          <Form.Control  size="lg"type="text" placeholder="Select Number" />
+        </Form.Group>
+        
+        <br></br>
+
+        <div class="button">
+          <Button variant="secondary" size="lg">
+            Submit
+          </Button>
+        </div>
+
+      </Form>
     </div>
   );
 }
